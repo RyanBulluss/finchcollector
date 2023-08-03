456a603c7037dae12b79c80ec512d41928cbba1c
ryan <rbulluss2000@gmail.com>
2023-08-03 11:44:17 +0100

    Lab complete

--- main_app/models.py
+++ main_app/models.py
@@ -8,11 +8,26 @@ MEALS = (
     ('D', 'Dinner'),
 )
 
+
+
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('toys_detail', kwargs={'pk': self.id})
+
+
 class Finch(models.Model):
     name = models.CharField(max_length=100)
     color = models.CharField(max_length=100)
     height = models.IntegerField()
     age = models.IntegerField()
+
+    toys = models.ManyToManyField(Toy)
     
     def __str__(self):
         return self.name
